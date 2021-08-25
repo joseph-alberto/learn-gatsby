aae7543234cbaae100220f2213949a336f9bba04
Joseph Alberto <hyunrenz@gmail.com>
2021-08-25 11:12:36 +0700

    Add hyperlink to /

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'gatsby'
 
 const AboutPage = () => {
     return (
@@ -6,6 +7,7 @@ const AboutPage = () => {
             <title>About Page</title>
             <h1>About Me!</h1>
             <p>Hello there, I'm Joseph and this is my first Gatsby site!</p>
+            <Link to='/'>Home</Link>
         </main>
     )
 }
