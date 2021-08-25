b03070bab7c231bc3fb961e085b29003fa24387c
Joseph Alberto <hyunrenz@gmail.com>
2021-08-25 11:01:11 +0700

    Create about.js

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About Page</title>
+            <h1>About Me!</h1>
+            <p>Hello there, I'm Joseph and this is my first Gatsby site!</p>
+        </main>
+    )
+}
+
+export default AboutPage
